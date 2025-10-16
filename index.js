0f10bef036b93114680875a82233c3c72b524b15
Ruqiya <ruqiyageelle@gmail.com>
2025-10-16 03:33:06 +0100

    fixed error

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ http.createServer(function(req, res) {
             <title>All About me!</title>
         </head>
         <body>
-           <h1><b>All About Me!/b></h1>
+           <h1>All About Me!</h1>
            <h2>My name is Ruqiya currently studying Creative Computing at Goldsmiths University of London and I have just started my second year. Excited to delve even deeper and gain some knowledge.</h2>
            <p>I orginally didnt have any experince with computers so when I did start First Year it was a bit overwhelming but I have learnt so much and made stuff that I never beleieved I could make</p>
            <h6>Lets hope I can make really cool stuff.</h6>
@@ -22,3 +22,5 @@ http.createServer(function(req, res) {
 }).listen(port, function() {
     console.log(`Node server is running on port ${port}...`);
 });
+
+
